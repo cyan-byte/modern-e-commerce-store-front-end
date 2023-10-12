8f28d780e56d6f27c34dc0b7817ff76aa2dd41a9
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-10-12 12:42:40 -0400

    cart styling

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,18 +5,18 @@ export default function CartItem() {
     <div className="cart-item-container">
         <div className="cart-item-left">
       <div className="cart-item-image-container">
-        <img src="pexels-vlada-karpovich-7902913.png" />
+        <img src="pexels-vlada-karpovich-7902913.png" className="mammoth-vase-cart-item"/>
       </div>
       <div className="delete-and-update-row">
-        <img src="trash-icon.png"/>
+        <img src="trash-icon.png" className="trash-icon"/>
         <p>1</p>
-        <img src="green-plus-icon.png"/>
+        <img src="green-plus-icon.png" className="green-plus-icon"/>
       </div>
       </div>
       <div className="cart-item-right">
-        <h2>Item Title</h2>
-        <p>price</p>
-        <p>color</p>
+        <h2>Mammoth Vase</h2>
+        <p>price: <span className="product-home-price">$62</span> </p>
+        <p>color: deep sand</p>
         <div className="cart-delete-button">Delete</div>
       </div>
     </div>
